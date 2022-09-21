0106c65f9e87f21c71e8de67965109c8826ae10f
RikikiB <rikkib27007@yahoo.com>
2022-09-21 14:19:12 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-=======
 import "./App.css";
 import React from "react";
 // import { Player } from "video-react";
@@ -128,5 +102,4 @@ function App() {
     </>
   );
 }
->>>>>>> 35b2662f1947719d79c879d08b3f0a81236a7026
 export default App;
