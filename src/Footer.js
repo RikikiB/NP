2dd5d3e7f01b0f77b9b827aae8cfacd141c7c73a
RikikiB <rikkib27007@yahoo.com>
2022-09-24 09:24:07 -0700

    Package.json delete footer code added

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return (
+        <div className="Footer">
+            <ul>HOME</ul>
+            <ul>VIDEO</ul>
+            <ul>CHAT</ul>
+            <ul>FRIENDS</ul>
+
+        </div>
+    )
+}
