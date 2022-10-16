0df0fb3f40f79a2609fa533d216b977868bbdb5c
Rekishia Brooks <87107471+RikikiB@users.noreply.github.com>
2022-10-15 17:22:23 -0700

    Updates

---
+++ src/Components/IG.js
@@ -0,0 +1,7 @@
+https://api.instagram.com/oauth/authorize
+  ?client_id="636102944665461"
+  &redirect_uri="https://github.com/RikikiB/NP.git"
+  &scope=user_profile,user_media
+  &response_type=code
+
+      
